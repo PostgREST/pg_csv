a8d628588a9464a780765a646814d78c153c3dfe
steve-chavez <stevechavezast@gmail.com>
2026-08-09 20:58:41 -0500

    bench: only one csv_agg bench
    Having two is unnecessary, it was done this way to ensure no regression
    happened between the call to csv_agg with args and without args; but now
    that we have a comparison between csv_agg and custom_csv we'll know if a
    regression happens just by comparing these two.

--- bench/csv_agg.sql
+++ bench/csv_agg.sql
@@ -1,5 +1,5 @@
 \set lim random(1000, 2000)
 
-select csv_agg(t) from (
+select csv_agg(t, csv_options(delimiter:='|', bom:=true, header:=false, nullstr:='<NULL>')) from (
   select * from orders_customers limit :lim
 ) as t;
